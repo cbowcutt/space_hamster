9130aace93fc236921a392d4586049f1c356f35e
PCB\cbowcutt <cbowcutt@pcb.com>
2019-07-01 13:38:44 -0600

    helper function configAnimatable()

--- hamster_sprite_builder.js
+++ hamster_sprite_builder.js
@@ -23,33 +23,33 @@ function HamsterSpriteBuilder() {
 		var hamster_sprite = new PlayableCharacter();
 		hamster_sprite.width = this.tile_width;
 		hamster_sprite.height = this.tile_height;
-		hamster_sprite.add_animation('walking_down', u.sprite(u.filmstrip('hamster_down', 128, 128)));
-		hamster_sprite.add_animation('walking_left', u.sprite(u.filmstrip('hamster_left', 128,128)));
-		hamster_sprite.add_animation('walking_up', u.sprite(u.filmstrip('hamster_up', 128, 128)));
-		hamster_sprite.add_animation('walking_right', u.sprite(u.filmstrip('hamster_right', 128, 128)));
-		hamster_sprite.add_animation('walking_down_hurt', u.sprite(u.filmstrip('hamster_down_hurt', 128, 128)));
-		hamster_sprite.add_animation('walking_left_hurt', u.sprite(u.filmstrip('hamster_left_hurt', 128,128)));
-		hamster_sprite.add_animation('walking_up_hurt', u.sprite(u.filmstrip('hamster_up_hurt', 128, 128)));
-		hamster_sprite.add_animation('walking_right_hurt', u.sprite(u.filmstrip('hamster_right_hurt', 128, 128)));
-		hamster_sprite.set_current_animation('walking_down');
-		hamster_sprite.current_animation_id = "walking_down";
-		hamster_sprite.animate(scene, id);
+		
+		var animations = {}
+		
+		animations['walking_down'] = u.sprite(u.filmstrip('hamster_down', 128, 128));
+		animations['walking_left'] = u.sprite(u.filmstrip('hamster_left', 128,128));
+		animations['walking_up'] = u.sprite(u.filmstrip('hamster_up', 128, 128));
+		animations['walking_right'] = u.sprite(u.filmstrip('hamster_right', 128, 128));
+		animations['walking_down_hurt'] = u.sprite(u.filmstrip('hamster_down_hurt', 128, 128));
+		animations['walking_left_hurt'] = u.sprite(u.filmstrip('hamster_left_hurt', 128,128));
+		animations['walking_up_hurt'] = u.sprite(u.filmstrip('hamster_up_hurt', 128, 128));
+		animations['walking_right_hurt'] =  u.sprite(u.filmstrip('hamster_right_hurt', 128, 128));
+		hamster_sprite = configAnimatable(hamster_sprite, this.tile_width, this.tile_height, animations, "walking_down");
 		Player = hamster_sprite;
 	}
 	
 	this.createRat = function(name, id) {
 		let u = new SpriteUtilities(PIXI);
 		var sprite = new RatNPC();
-		sprite.width = 32;
-		sprite.height = 32;
 		
-		sprite.add_animation('walking_down', u.sprite(u.filmstrip('rat_down', 32, 32)));
-		sprite.add_animation('walking_left', u.sprite(u.filmstrip('rat_left', 32,32)));
-		sprite.add_animation('walking_up', u.sprite(u.filmstrip('rat_up', 32, 32)));
-		sprite.add_animation('walking_right', u.sprite(u.filmstrip('rat_right', 32, 32)));
-		sprite.set_current_animation('walking_down');
-
-		sprite.animate(scene, id);
+		var animations = {
+			"walking_down" : u.sprite(u.filmstrip('rat_down', 32, 32)),
+			"walking_left" : u.sprite(u.filmstrip('rat_left', 32,32)),
+			"walking_up"   : u.sprite(u.filmstrip('rat_up', 32, 32)),
+			"walking_right": u.sprite(u.filmstrip('rat_right', 32, 32))
+		};
+		
+		sprite = configAnimatable(sprite, 32, 32, animations, "walking_down");
 		return sprite;
 	}
 	
@@ -57,12 +57,8 @@ function HamsterSpriteBuilder() {
 	{
 		let u = new SpriteUtilities(PIXI);
 		var sprite = new Coin(x, y);
-		sprite.width = 32;
-		sprite.height = 32;
 		let texture = u.frame("coin_1", 0, 0, 32, 32);
-		sprite.add_animation("spinningCoin", u.sprite(u.filmstrip("coin_1", 32, 32)));
-		sprite.set_current_animation('spinningCoin');
-		sprite.animate(scene);
+		sprite = configAnimatable(sprite, 32, 32, {"spinningCoin": u.sprite(u.filmstrip("coin_1", 32, 32))}, "spinningCoin")
 		return sprite;
 	}
 	
@@ -70,11 +66,7 @@ function HamsterSpriteBuilder() {
 	{
 		let u = new SpriteUtilities(PIXI);
 		var sprite = new Weapon(character);
-		sprite.width = 32;
-		sprite.height = 32;
-		let texture = u.frame("sword", 0, 0, 32, 32);
-		sprite.add_animation("swing", u.sprite(u.filmstrip("sword", 32, 32)));
-		sprite.animate(scene);
+		sprite = configAnimatable(sprite, 32, 32,  {"swing": u.sprite(u.filmstrip("sword", 32, 32))}, "swing");
 		return sprite;
 	}
 	
@@ -84,16 +76,19 @@ function HamsterSpriteBuilder() {
 		for (var i = 0; i < healthCount; i++)
 		{
 			var sprite = new Heart(i * TILEWIDTH, 32);
-			sprite.width = 32;
-			sprite.height = 32;
-			sprite.add_animation('heart', u.sprite(u.filmstrip('heart', 32, 32)));
-			sprite.set_current_animation('heart');
-			sprite.current_animation.y = sprite.x;
-	    	sprite.current_animation.x = sprite.y;
-			sprite.animate(scene);
-			hearts.push(sprite);
+			hearts.push(configAnimatable(sprite, 32, 32, {"heart": u.sprite(u.filmstrip("heart", 32, 32))}, "heart"));
 		}
 		return hearts;
+	}
+	
+	function configAnimatable(sprite, width, height, animations, currentAnimation)
+	{
+		sprite.width = width;
+		sprite.height = height;
+		Object.keys(animations).forEach(key => sprite.add_animation(key, animations[key]));
+		sprite.set_current_animation(currentAnimation);
+		sprite.animate(scene);
+		return sprite;
 	}
 	
 	
